53118ddc0078207cd5cd8f5948322795fb91adc2
Timo <timjlah@gmail.com>
2016-07-20 17:06:38 +0300

    Wrapper for bitcount intrinsics for msvc and g++.

--- util.h
+++ util.h
@@ -17,4 +17,22 @@ inline unsigned countTrailingZeros(unsigned x)
     #endif
 }
 
+inline unsigned bitCount(unsigned x)
+{
+    #if _MSC_VER
+    return __popcnt(x);
+    #else
+    return __builtin_popcount(x);
+    #endif
+}
+
+inline unsigned bitCount(unsigned long long x)
+{
+    #if _MSC_VER
+    return __popcnt64(x);
+    #else
+    return __builtin_popcount(x);
+    #endif
+}
+
 #endif // UTIL_H
