f89961f83888ea5caa9d11633dafe68331429c41
Timo <timjlah@gmail.com>
2016-07-21 23:25:26 +0300

    Added a class for storing/parsing a list of Holdem starting hands.

---
+++ CardRange.cpp
@@ -0,0 +1,230 @@
+#include "CardRange.h"
+#include <locale>
+#include <algorithm>
+#include <cassert>
+
+// Construct empty.
+CardRange::CardRange()
+{
+}
+
+// Construct from expression.
+CardRange::CardRange(const std::string& text)
+{
+    // Turn to lowercase and remove spaces and control chars.
+    std::locale loc;
+    std::string s;
+    for (char c:text) {
+        if (std::isgraph(c, loc))
+            s.push_back(std::tolower(c, loc));
+    }
+
+    const char* p = s.data();
+    while(parseHand(p) && parseChar(p, ','))
+          ;
+
+    if (s == "random")
+        addAll();
+
+    removeDuplicates();
+}
+
+// Construct from vctor.
+CardRange::CardRange(const std::vector<std::array<char,2>>& combos)
+{
+    for (auto& combo : combos)
+        addCombo(combo[0], combo[1]);
+    removeDuplicates();
+}
+
+// Card mask from a string.
+uint64_t CardRange::getCardMask(const std::string& text)
+{
+    std::locale loc;
+    std::string s;
+    for (char c:text) {
+        if (std::isgraph(c, loc))
+            s.push_back(std::tolower(c, loc));
+    }
+
+    uint64_t cards = 0;
+    for (size_t i = 0; i < s.size() - std::min<size_t>(1, s.size()); i += 2) {
+        unsigned rank = charToRank(s[i]);
+        unsigned suit = charToSuit(s[i + 1]);
+        if (rank == ~0 || suit == ~0)
+            break;
+        unsigned card = 4 * rank + suit;
+        cards |= 1ull << card;
+    }
+
+    return cards;
+}
+
+// Parses a single hand and advances pointer p.
+bool CardRange::parseHand(const char*&p)
+{
+    const char* backtrack = p;
+
+    bool explicitSuits = false;
+    unsigned r1, r2, s1, s2;
+    if (!parseRank(p, r1))
+        return false;
+    explicitSuits = parseSuit(p, s1);
+    if (!parseRank(p, r2)) {
+        p = backtrack;
+        return false;
+    }
+    if (explicitSuits && !parseSuit(p, s2)) {
+        p = backtrack;
+        return false;
+    }
+
+    if (explicitSuits) {
+        unsigned c1 = 4 * r1 + s1, c2 = 4 * r2 + s2;
+        if (c1 == c2) {
+            p = backtrack;
+            return false;
+        }
+        addCombo(c1, c2);
+    } else if (!explicitSuits) {
+        bool suited = true, offsuited = true;
+        if (parseChar(p, 'o'))
+            suited = false;
+        else if (parseChar(p, 's'))
+            offsuited = false;
+        if (parseChar(p, '+'))
+            addCombosPlus(r1, r2, suited, offsuited);
+        else
+            addCombos(r1, r2, suited, offsuited);
+    }
+
+    return true;
+}
+
+// Parse a rank from 2 to A.
+bool CardRange::parseRank(const char*&p, unsigned& rank)
+{
+    rank = charToRank(*p);
+    if (rank == ~0u)
+        return false;
+    ++p;
+    return true;
+}
+
+// Parse a suit (s, h, c, d)
+bool CardRange::parseSuit(const char*&p, unsigned& suit)
+{
+    suit = charToSuit(*p);
+    if (suit == ~0u)
+        return false;
+    ++p;
+    return true;
+}
+
+// Try to read a specific character.
+bool CardRange::parseChar(const char*&p, char c)
+{
+    if (*p == c) {
+        ++p;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Add combos for specific ranks.
+void CardRange::addCombos(unsigned rank1, unsigned rank2, bool suited, bool offsuited)
+{
+    if (suited && rank1 != rank2) {
+        for (unsigned suit = 0; suit < 4; ++suit)
+            addCombo(4 * rank1 + suit, 4 * rank2 + suit);
+    }
+    if (offsuited) {
+        for (unsigned suit1 = 0; suit1 < 4; ++suit1)
+            for (int suit2 = suit1 + 1; suit2 < 4; ++suit2) {
+                addCombo(4 * rank1 + suit1, 4 * rank2 + suit2);
+                if (rank1 != rank2)
+                    addCombo(4 * rank1 + suit2, 4 * rank2 + suit1);
+            }
+    }
+}
+
+// Add range of hands defined by the "+" suffix.
+void CardRange::addCombosPlus(unsigned rank1, unsigned rank2, bool suited, bool offsuited)
+{
+    if (rank1 == rank2){
+        for (unsigned r = rank1; r < 13; ++r)
+            addCombos(r, r, suited, offsuited);
+    } else {
+        if (rank1 < rank2)
+            std::swap(rank1, rank2);
+        for (unsigned r = rank2; r < rank1; ++r)
+            addCombos(rank1, r, suited, offsuited);
+    }
+}
+
+void CardRange::addAll()
+{
+    for (unsigned c1 = 0; c1 < 52; ++c1)
+        for (unsigned c2 = 0; c2 < c1; ++c2)
+            addCombo(c1, c2);
+}
+
+void CardRange::addCombo(unsigned c1, unsigned c2)
+{
+    assert(c1 != c2);
+    if (c1 >> 2 < c2 >> 2 || c1 >> 2 == c2 >> 2 && (c1 & 3) < (c2 & 3))
+        std::swap(c1, c2);
+    mCombinations.emplace_back(std::array<char,2>{(char)c1, (char)c2});
+}
+
+// Removes duplicate combos.
+void CardRange::removeDuplicates()
+{
+    std::sort(mCombinations.begin(), mCombinations.end(), [](const std::array<char,2>& lhs,
+              const std::array<char,2>& rhs){
+        if (lhs[0] >> 2 != rhs[0] >> 2)
+            return lhs[0] >> 2 < rhs[0] >> 2;
+        if (lhs[1] >> 2 != rhs[1] >> 2)
+            return lhs[1] >> 2 < rhs[1] >> 2;
+        if ((lhs[0] & 3) != (rhs[0] & 3))
+            return (lhs[0] & 3) < (rhs[0] & 3);
+        return (lhs[1] & 3) < (rhs[1] & 3);
+    });
+    auto last = std::unique(mCombinations.begin(), mCombinations.end(), [](const std::array<char,2>& lhs,
+                            const std::array<char,2>& rhs){
+        return lhs[0] == rhs[0] && lhs[1] == rhs[1];
+    });
+    mCombinations.erase(last, mCombinations.end());
+}
+
+unsigned CardRange::charToRank(char c)
+{
+    switch(c) {
+        case 'a': return 12;
+        case 'k': return 11;
+        case 'q': return 10;
+        case 'j': return 9;
+        case 't': return 8;
+        case '9': return 7;
+        case '8': return 6;
+        case '7': return 5;
+        case '6': return 4;
+        case '5': return 3;
+        case '4': return 2;
+        case '3': return 1;
+        case '2': return 0;
+        default: return ~0u;
+    }
+}
+
+unsigned CardRange::charToSuit(char c)
+{
+    switch(c) {
+        case 's': return 0;
+        case 'h': return 1;
+        case 'c': return 2;
+        case 'd': return 3;
+        default: return ~0u;
+    }
+}
---
+++ CardRange.h
@@ -0,0 +1,58 @@
+#ifndef CARDRANGE_H
+#define CARDRANGE_H
+
+#include <string>
+#include <vector>
+#include <array>
+#include <cstdint>
+
+// Stores a set of unique starting hands for Texas Holdem.
+class CardRange
+{
+public:
+    // Constructs an empty range.
+    CardRange();
+
+    // Constructs a range from an expression. Supported syntax:
+    // K4 : all suited and offsuited combos with specified ranks
+    // K4s : suited combos
+    // K4o : offsuited combos
+    // Kc4d : specific suits
+    // K4o+ : specified hand and all similar hands with a better kicker (K4 to KQ)
+    // 44+ : pocket pair and all higher pairs
+    // K4+,Q8s,84 : multiple hands can be combined with comma
+    // random : all hands
+    // Spaces and non-matching characters in the end are ignored. The expressions are case-insensitive.
+    CardRange(const std::string& text);
+
+    // Constructs a range from a list of two-card combinations.
+    CardRange(const std::vector<std::array<char,2>>& combos);
+
+    // Returns a list of card combinations belonging to this range. Guarantees that there are no duplicates.
+    // Cards in each combo are ordered so that the bigger rank is always first. The whole vector is sorted in the
+    // following order: 1) rank of first card 2) rank of second card 3) suit of first card 4) suit of second card
+    const std::vector<std::array<char,2>>& combinations() const
+    {
+        return mCombinations;
+    }
+
+    // Returns a 64-bit bitmask of cards from a string like "2c8hAh".
+    static uint64_t getCardMask(const std::string& text);
+
+private:
+    bool parseHand(const char*&p);
+    bool parseRank(const char*&p, unsigned& rank);
+    bool parseSuit(const char*&p, unsigned& suit);
+    bool parseChar(const char*&p, char c);
+    void addAll();
+    void addCombos(unsigned rank1, unsigned rank2, bool suited, bool offsuited);
+    void addCombosPlus(unsigned rank1, unsigned rank2, bool suited, bool offsuited);
+    void addCombo(unsigned c1, unsigned c2);
+    void removeDuplicates();
+    static unsigned charToRank(char c);
+    static unsigned charToSuit(char c);
+
+    std::vector<std::array<char,2>> mCombinations;
+};
+
+#endif // CARDRANGE_H
